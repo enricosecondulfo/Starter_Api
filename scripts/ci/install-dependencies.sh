d734cc9cd32941098874342eddfa2903576b8fdc
Enrico Secondulfo <enrico.secondulfo85@gmail.com>
2016-05-31 23:55:07 +0200

    Modifing circle.yml

--- scripts/ci/install-dependencies.sh
+++ scripts/ci/install-dependencies.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-if [ ! -e mongodb-linux-x86_64-3.2.6/bin ]; then
+if [ ! -e mongodb-linux-x86_64-ubuntu1404-3.2.6/bin ]; then
     sudo service mongod stop
     curl -L -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1404-3.2.6.tgz
     tar xvzf mongodb-linux-x86_64-ubuntu1404-3.2.6.tgz
