e4c81dfdb227a62cf273e4cbe7934ffddaf91163
Enrico Secondulfo <enrico.secondulfo85@gmail.com>
2016-06-01 00:01:43 +0200

    Modifing circle.yml

--- scripts/ci/install-dependencies.sh
+++ scripts/ci/install-dependencies.sh
@@ -11,12 +11,14 @@ if [ ! -e mongodb-linux-x86_64-ubuntu1404-3.2.6 ]; then
     sudo service mongod start
 fi
 
-gradle_version=$(echo $($GRADLE_HOME/bin/gradle -version) | awk '{ print $3; }')
+echo $GRADLE_HOME
 
-if [[ "$gradle_version" < "2.10" ]]; then
-      sudo add-apt-repository ppa:cwchien/gradle -y
-      sudo apt-get clean
-      sudo apt-get update
-      sudo apt-get --purge remove gradle
-      sudo apt-get install gradle
-fi
+#gradle_version=$(echo $($GRADLE_HOME/bin/gradle -version) | awk '{ print $3; }')
+#
+#if [[ "$gradle_version" < "2.10" ]]; then
+#      sudo add-apt-repository ppa:cwchien/gradle -y
+#      sudo apt-get clean
+#      sudo apt-get update
+#      sudo apt-get --purge remove gradle
+#      sudo apt-get install gradle
+#fi
