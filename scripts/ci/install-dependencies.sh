e0caa75eb6a6a7626ec9cf9b4570e1292f77f937
Enrico Secondulfo <enrico.secondulfo85@gmail.com>
2016-05-31 23:41:03 +0200

    Modifing circle.yml

--- scripts/ci/install-dependencies.sh
+++ scripts/ci/install-dependencies.sh
@@ -7,7 +7,8 @@ if [ ! -e mongodb-linux-x86_64-3.2.6/bin ]; then
     curl -L -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1404-3.2.6.tgz
     tar xvzf mongodb-linux-x86_64-ubuntu1404-3.2.6.tgz
     sudo mv mongodb-linux-x86_64-ubuntu1404-3.2.6/bin/* /usr/bin/
-    sudo service mongodb start
+    cd /usr/bin
+    sudo mongod
 fi
 
 gradle_version = $(echo $($GRADLE_HOME/bin/gradle -version) | awk '{ print $3; }')
